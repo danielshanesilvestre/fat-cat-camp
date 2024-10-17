409d4942df5096b216770ec7bbd7d5496bf453f3
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-10-17 10:08:42 -0500

    Add option to create owner

--- lib/helpers.py
+++ lib/helpers.py
@@ -3,11 +3,38 @@ from models.owner import Owner;
 from models.cat import Cat;
 
 def owners_menu():
-    print("Viewing owners:")
-    for owner in Owner.get_all():
-        
-        print(f"- {owner.name}, {owner.phone_number}")
-        pass
+    exit = False
+
+    while not exit:
+        print("Viewing owners:")
+        for owner in Owner.get_all():
+            print(f"- {owner.name}, {owner.phone_number}")
+            pass
+        print("Please select an option:")
+        print("b - Go back to previous menu")
+        print("c - Register an owner")
+        print("d - Delete an owner")
+        choice = input("> ")
+        if choice == "b":
+            exit = True
+        elif choice == "c":
+            create_owner()
+        elif choice == "d":
+            delete_owner()
+        else:
+            print("Invalid choice")
+
+def create_owner():
+
+    print("Please enter the new owner's name:")
+    name = input("> ")
+    print("Enter the new owner's phone number:")
+    phone_number = input("> ")
+    owner = Owner.create(name, phone_number)
+    print(f"Registered: {owner.name}, {owner.phone_number}")
+
+    pass
+
 
 def cats_menu():
     print("Viewing cats:")
